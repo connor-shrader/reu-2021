e1e4abb899318ca028c548e37c17e6555a453609
connor-shrader <connorshrader@gmail.com>
2021-07-05 16:38:20 -0400

    Created simple OLS figure

---
+++ code/figures/ols-figure.r
@@ -0,0 +1,44 @@
+library(ggplot2)
+library(rstudioapi)
+
+setwd(dirname(getActiveDocumentContext()$path))
+source("themes.r")
+
+set.seed(1)
+x <- sample(seq(0, 4, length.out = 101), 50)
+eps <- rnorm(50, mean = 0, sd = 0.5)
+y <- x + eps
+
+dat <- data.frame(x = x, y = y)
+
+model <- lm(y ~ x, data = dat)
+coefs <- coef(model)
+
+dat$yhat <- coefs[1] + coefs[2] * dat$x
+
+library(ggplot2)
+
+ols <- ggplot(data = dat, mapping = aes(x = x, y = y)) +
+  geom_segment(aes(x = x, y = y, xend = x, yend = yhat), color = "red") +
+  geom_point() + 
+  geom_abline(slope = coefs[2], intercept = coefs[1]) +
+  reu_border
+
+ggsave(
+  filename = "ols.png",
+  path = "./images",
+  plot = ols,
+  type = "cairo-png",
+  width = 10,
+  height = 6,
+  unit = "in"
+)
+
+ggsave(
+  filename = "ols.eps",
+  path = "./images",
+  plot = ols,
+  width = 10,
+  height = 6,
+  unit = "in"
+)
