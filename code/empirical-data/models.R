1b1925443571fd469979315fd8445307e9642ecc
Gabriel Ackall <ackall2020@gmail.com>
2021-07-08 22:38:44 -0400

    empirical data analysis

--- code/empirical-data/models.R
+++ code/empirical-data/models.R
@@ -9,6 +9,10 @@ library(e1071)
 library(caret) #for cross validation data splitting
 library(tidyverse) #data cleaning
 
+# prevent stack overflow
+options(expressions = 5e5)
+memory.limit(size=8000000)
+
 #set seed
 set.seed(12345)
 
@@ -133,6 +137,169 @@ for (fold in seq(cv_folds)) {
   runtimes[[fold]][["mcp"]] <- mcp_time
   cv.mse[[fold]][["mcp"]] <- calc_mse(mcp, test_dat)
   
+  # XGBoost Grid Search
+  # Separate data into x and y matrices. This is needed to run xgboost.
+  train_x_data <- as.matrix(train_dat[, -1])
+  train_y_data <- as.matrix(train_dat[, 1])
+  
+  xgb_time <- system.time({
+    xgb_hyper_grid <- expand.grid(
+      eta = c(.1, .3, .5),  # learning rate
+      max_depth = c(1, 3, 7), # maximum depth of each tree
+      optimal_trees = 0,               # a place to dump results
+      min_RMSE = 0                     # a place to dump results
+    )
+    
+    # lapply(1:nrow(xgb_hyper_grid), function(i) {
+    #   # create parameter list
+    #   params <- list(
+    #     eta = xgb_hyper_grid$eta[i],
+    #     max_depth = xgb_hyper_grid$max_depth[i],
+    #     min_child_weight = xgb_hyper_grid$min_child_weight[i],
+    #     subsample = xgb_hyper_grid$subsample[i],
+    #     colsample_bytree = xgb_hyper_grid$colsample_bytree[i]
+    #   )
+    # 
+    #   # train model
+    #   xgb.tune <- xgb.cv(
+    #     params = params,
+    #     data = train_x_data,
+    #     label = train_y_data,
+    #     nrounds = 1000,
+    #     nfold = 5,
+    #     objective = "reg:squarederror",  # for regression models
+    #     verbose = 0,               # silent,
+    #     early_stopping_rounds = 10, # stop if no improvement for 10 consecutive trees
+    #   )
+    # 
+    #   # add min training error and trees to grid
+    #   xgb_hyper_grid$optimal_trees[i] <- which.min(xgb.tune$evaluation_log$test_rmse_mean)
+    #   xgb_hyper_grid$min_RMSE[i] <- min(xgb.tune$evaluation_log$test_rmse_mean)
+    # })
+    
+    # grid search
+    for(i in 1:nrow(xgb_hyper_grid)) {
+      
+      # create parameter list
+      params <- list(
+        eta = xgb_hyper_grid$eta[i],
+        max_depth = xgb_hyper_grid$max_depth[i]
+      )
+      
+      # train model
+      xgb.tune <- xgb.cv(
+        params = params,
+        min_child_weight = 1, # default
+        subsample = 1, 
+        colsample_bytree = 1,
+        data = train_x_data,
+        label = train_y_data,
+        nrounds = 1000,
+        nfold = 5,
+        objective = "reg:squarederror",  # for regression models
+        verbose = 0,               # silent,
+        early_stopping_rounds = 10 # stop if no improvement for 10 consecutive trees
+      )
+      
+      # add min training error and trees to grid
+      xgb_hyper_grid$optimal_trees[i] <- which.min(xgb.tune$evaluation_log$test_rmse_mean)
+      xgb_hyper_grid$min_RMSE[i] <- min(xgb.tune$evaluation_log$test_rmse_mean)
+    }
+    
+    xgb_best_grid <- xgb_hyper_grid %>%
+      dplyr::arrange(min_RMSE)
+    
+    xgb_best_params <- list(
+      eta = xgb_best_grid$eta[1],
+      max_depth = xgb_best_grid$max_depth[1]
+    )
+    
+    train_set <- xgb.DMatrix(data = as.matrix(train_dat[, -1]), label = as.matrix(train_dat[, 1]))
+    
+    # train best model
+    xgb.best <- xgboost(
+      params = xgb_best_params,
+      data = train_set,
+      nrounds = 1000,
+      objective = "reg:squarederror",  # for regression models
+      verbose = 0,               # silent,
+      early_stopping_rounds = 10 # stop if no improvement for 10 consecutive trees
+    )
+  })
+  models[[fold]][["gbm"]] <- xgb.best
+  runtimes[[fold]][["gbm"]] <- xgb_time
+  cv.mse[[fold]][["gbm"]] <- calc_mse(xgb.best, test_dat)
+  
+  # Random Forest Grid Search
+  rf_time <- system.time({
+    p <- length(train_dat)
+    
+    rf_hyper_grid <- expand.grid(
+      mtry       = c(floor(sqrt(p)), floor(p / 3), floor(p/2)),  # Predictors per tree
+      n.trees    = c(300, 500, 700),
+      OOB_RMSE   = 0
+    )
+    
+    # lapply(1:nrow(rf_hyper_grid), function(i) {
+    #   # train model
+    #   rf_model <- ranger(
+    #     formula         = y ~ ., 
+    #     data            = dat, 
+    #     num.trees       = rf_hyper_grid$n.trees[i],
+    #     mtry            = rf_hyper_grid$mtry[i],
+    #     min.node.size   = rf_hyper_grid$node_size[i],
+    #     sample.fraction = rf_hyper_grid$sampe_size[i],
+    #     seed            = 123
+    #   )
+    #   
+    #   # add OOB error to grid
+    #   rf_hyper_grid$OOB_RMSE[i] <- sqrt(rf_model$prediction.error)
+    # })
+    
+    for(i in 1:nrow(rf_hyper_grid)) {
+      
+      # train model
+      rf_model <- randomForest(
+        x               = train_dat[,-1],
+        y               = train_dat[,1],
+        ntree           = rf_hyper_grid$n.trees[i],
+        mtry            = rf_hyper_grid$mtry[i],
+        nodesize        = 5
+      )
+      
+      # add OOB error to grid
+      rf_hyper_grid$OOB_RMSE[i] <- sqrt(mean(rf_model$mse))
+    }
+    
+    rf_best_grid <- rf_hyper_grid %>% 
+      dplyr::arrange(OOB_RMSE)
+    
+    # train best model
+    best_rf_model <- randomForest(
+      x               = train_dat[,-1],
+      y               = train_dat[,1], 
+      ntree           = rf_best_grid$n.trees[1],
+      mtry            = rf_best_grid$mtry[1],
+      nodesize        = 5
+    )
+  })
+  models[[fold]][["rf"]] <- best_rf_model
+  runtimes[[fold]][["rf"]] <- rf_time
+  cv.mse[[fold]][["rf"]] <- calc_mse(best_rf_model, test_dat)
+  
+  # Support Vector Machine
+  svm_time <- system.time({
+    svm_tune <- tune.svm(y ~ ., data = train_dat,
+                         epsilon = seq(0.1, 0.5, 0.2),
+                         cost = c(0.5, 1, 2)
+    )
+    
+    svm_model <- svm_tune$best.model
+  })
+  
+  models[[fold]][["svm"]] <- svm_model
+  runtimes[[fold]][["svm"]] <- svm_time
+  cv.mse[[fold]][["svm"]] <- calc_mse(svm_model, test_dat)
 }
 
 # create dataframe of cross validation values
