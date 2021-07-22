ff89e612a05def6278c5c7d5bb6067a3c4cb82b6
connor-shrader <connorshrader@gmail.com>
2021-07-22 17:43:43 -0400

    Added citation for biglasso

--- code/figures/random-forest.r
+++ code/figures/random-forest.r
@@ -9,4 +9,40 @@ source("save-plot.r")
 
 set.seed(1)
 data(iris)
-fit <- randomForest(Species ~ ., data = iris)
+fit <- randomForest(Species ~ ., data = iris, ntree = 3)
+plot(fit)
+
+t <- getTree(fit, 1, labelVar = TRUE)
+class(t)
+
+to.dendrogram <- function(dfrep,rownum=1,height.increment=0.1){
+  
+  if(dfrep[rownum,'status'] == -1){
+    rval <- list()
+    
+    attr(rval,"members") <- 1
+    attr(rval,"height") <- 0.0
+    attr(rval,"label") <- dfrep[rownum,'prediction']
+    attr(rval,"leaf") <- TRUE
+    
+  }else{##note the change "to.dendrogram" and not "to.dendogram"
+    left <- to.dendrogram(dfrep,dfrep[rownum,'left daughter'],height.increment)
+    right <- to.dendrogram(dfrep,dfrep[rownum,'right daughter'],height.increment)
+    rval <- list(left,right)
+    
+    attr(rval,"members") <- attr(left,"members") + attr(right,"members")
+    attr(rval,"height") <- max(attr(left,"height"),attr(right,"height")) + height.increment
+    attr(rval,"leaf") <- FALSE
+    attr(rval,"edgetext") <- dfrep[rownum,'split var']
+    #To add Split Point in Dendrogram
+    #attr(rval,"edgetext") <- paste(dfrep[rownum,'split var'],"\n<",round(dfrep[rownum,'split point'], digits = 2),"=>", sep = " ")
+  }
+  
+  class(rval) <- "dendrogram"
+  
+  return(rval)
+}
+
+tree <- getTree(fit,1,labelVar=TRUE)
+
+d <- to.dendrogram(tree)
