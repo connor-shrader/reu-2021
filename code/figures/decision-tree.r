ca9a9aef1d3ed4d5a62c902058bf55066c5b8446
Connor1736 <connorshrader@gmail.com>
2021-07-23 14:41:07 -0400

    Reworked decision tree figure

--- code/figures/decision-tree.r
+++ code/figures/decision-tree.r
@@ -3,20 +3,27 @@ library(rstudioapi)
 library(faux)
 library(rpart)
 library(rpart.plot)
+library(glmnet)
 
 setwd(dirname(getActiveDocumentContext()$path))
 source("themes.r")
 source("save-plot.r")
 
 set.seed(1)
-data(iris)
-fit <- rpart(Species ~ ., data = iris, method = "class")
+load("../empirical-data/simulation-environment.RData")
+lasso_coef <- models[[1]]$lasso %>% coef() %>% as.matrix()
+lasso_coef[, 1] <- sort(lasso_coef[, 1], decreasing = TRUE)
+var_names <- names(lasso_coef[2:5, 1])
+
+cancer_df_mini <- cancer_df[, c("y", var_names)]
+
+fit <- rpart(y ~ ., data = cancer_df_mini, method = "anova", maxdepth = 4)
 
 setEPS()
 postscript("images/decision-tree.eps")
-rpart.plot(fit, type = 5, extra = 0, legend.y = -1, cex = 1.8)
+rpart.plot(fit, type = 0, extra = 1, legend.y = -1)
 dev.off()
 
 png(file = "images/decision-tree.png")
-rpart.plot(fit, type = 5, extra = 0, legend.y = -1, cex = 1.8)
+rpart.plot(fit, type = 0, extra = 1, legend.y = -1)
 dev.off()
