a0e6c7d7b38732a972b7448818b5b1b8d0a7d8c8
Connor1736 <connorshrader@gmail.com>
2021-10-16 14:22:47 -0400

    Reworked facet plots to scale

--- code/figures/facet-plots.r
+++ code/figures/facet-plots.r
@@ -45,28 +45,19 @@ plot_metric_old <- function(data, metric, facet, ...) {
     )
 }
 
-newer_plot_metric <- function(data, metric, ...) {
+newer_plot_metric <- function(data, metric, small = FALSE, rescale = FALSE, ...) {
+  
+  
   dat <<- subset_data(data, ...)
   
-  if (metric == "mean_train_mse" || metric == "mean_test_mse") {
-    max1 <<- max(max(dat[dat$st_dev == 1,]$mean_train_mse),
-                 max(dat[dat$st_dev == 1,]$mean_test_mse))
-    min1 <<- 0
-    max3 <<- max(max(dat[dat$st_dev == 3,]$mean_train_mse),
-                 max(dat[dat$st_dev == 3,]$mean_test_mse))
-    min3 <<- 0
-    max6 <<- max(max(dat[dat$st_dev == 6,]$mean_train_mse),
-                 max(dat[dat$st_dev == 6,]$mean_test_mse))
-    min6 <<- 0
+  metric <- paste("mean_", metric, sep = "")
+  
+  if (metric == "mean_sensitivity" || metric == "mean_specificity") {
+    
+    dat <- dat[!dat$model_name %in% c("fm", "ridge", "gbm", "rf", "svm"), ]
   }
   
-  df2 <- data.frame(model_name = rep("OLS", 6), 
-                    type = rep("Independent", 6),
-                    st_dev = c("1", "1", "3", "3", "6", "6"))
-  df2[[metric]] <- c(max1, min1, max3, min3, max6, min6)
-  
-  df2 <<- df2
-  
+  # Replace model names with more readable names.
   old_names <- c("fm", "ab", "bb", "asb", "bsb", "af", "bf", "asf",
                  "bsf", "ridge", "lasso", "enet", "scad", "mcp", "gbm",
                  "rf", "svm")
@@ -75,7 +66,6 @@ newer_plot_metric <- function(data, metric, ...) {
                  "AIC SF", "BIC SF", "Ridge", "Lasso",
                  "E-net", "SCAD", "MCP", "XGBoost", "RF", "SVM")
   
-  # Replace model names with more readable names.
   dat$model_name <- mapvalues(dat$model_name,
                               from = old_names, to = new_names)
   
@@ -83,10 +73,6 @@ newer_plot_metric <- function(data, metric, ...) {
   old_st_dev <- c("1", "3", "6")
   new_st_dev <- c("sigma == 1", "sigma == 3", "sigma == 6")
   dat$st_dev <- mapvalues(dat$st_dev, from = old_st_dev, to = new_st_dev)
-  df2$st_dev <- mapvalues(df2$st_dev, from = old_st_dev, to = new_st_dev)
-  
-  df2$type <- factor(df2$type, levels = c("Independent", "Symmetric",
-                                          "Autoregressive", "Blockwise"))
   
   dat <- arrange(dat, corr)
   
@@ -96,10 +82,32 @@ newer_plot_metric <- function(data, metric, ...) {
                         to = c("Independent", "Symmetric",
                                "Autoregressive", "Blockwise"))
   
+  need_to_rescale <- rescale == TRUE && 
+    (metric == "mean_train_mse" || metric == "mean_test_mse")
+  if (need_to_rescale) {
+    max1 <<- max(max(dat[dat$st_dev == "sigma == 1",]$mean_train_mse),
+                 max(dat[dat$st_dev == "sigma == 1",]$mean_test_mse))
+    min1 <<- 0
+    max3 <<- max(max(dat[dat$st_dev == "sigma == 3",]$mean_train_mse),
+                 max(dat[dat$st_dev == "sigma == 3",]$mean_test_mse))
+    min3 <<- 0
+    max6 <<- max(max(dat[dat$st_dev == "sigma == 6",]$mean_train_mse),
+                 max(dat[dat$st_dev == "sigma == 6",]$mean_test_mse))
+    min6 <<- 0
+    
+    df2 <- data.frame(model_name = rep("Lasso", 6), 
+                      type = rep("Independent", 6),
+                      st_dev = c("1", "1", "3", "3", "6", "6"))
+    df2[[metric]] <- c(max1, min1, max3, min3, max6, min6)
+    df2$st_dev <- mapvalues(df2$st_dev, from = old_st_dev, to = new_st_dev)
+    
+    #df2$type <- factor(df2$type, levels = c("Independent", "Symmetric",
+    #                                        "Autoregressive", "Blockwise"))
+  }
+  
   ylabel <- NULL
   if (metric == "mean_train_mse") {
     ylabel <- "Mean Training MSE"
-    
   }
   else if (metric == "mean_test_mse") {
     ylabel <- "Mean Test MSE"
@@ -111,20 +119,38 @@ newer_plot_metric <- function(data, metric, ...) {
     ylabel <- expression(paste("Mean ", beta, "-specificity"))
   }
   
-  plt <- ggplot(data = dat) +
-    geom_point(mapping = aes_string(
-      x = "model_name",
-      y = metric,
-      color = "corr",
-      shape = "corr",
-      fill = "corr"
-    ))
+  plt <- ggplot(data = dat)
   
-  if (metric == "mean_train_mse" || metric == "mean_test_mse") {
-    plt <- plt + geom_point(mapping = aes_string(
-      x = "model_name",
-      y = metric),
-      data = df2, alpha = 0)
+  if (small) {
+    plt <- plt +
+      geom_point(
+        mapping = aes_string(
+          x = "model_name",
+          y = metric,
+          color = "corr",
+          shape = "corr",
+          fill = "corr"),
+        size = 4)
+  }
+  else {
+    plt <- plt +
+      geom_point(
+        mapping = aes_string(
+          x = "model_name",
+          y = metric,
+          color = "corr",
+          shape = "corr",
+          fill = "corr"),
+        size = 2)
+  }
+  
+  if(need_to_rescale) {
+    plt <- plt + 
+      geom_point(
+        mapping = aes_string(
+          x = "model_name",
+          y = metric),
+        data = df2, alpha = 0)
   }
   
   plt <- plt +
@@ -137,7 +163,17 @@ newer_plot_metric <- function(data, metric, ...) {
       panel.border = element_rect(color = "black", fill = NA, size = 0.2),
       panel.grid = element_line(color = "gray90"),
       strip.background = element_blank(),
+      axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 10),
+      axis.text.y = element_text(size = 10),
+      strip.text = element_text(size = 12),
+      axis.title = element_text(size = 16),
       legend.key = element_rect(fill = "white"),
+      legend.text = element_text(size = 12),
+      legend.title = element_text(size = 16)
+    )
+  
+  if(small == TRUE) {
+    plt <- plt + theme(
       axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 20),
       axis.text.y = element_text(size = 20),
       strip.text = element_text(size = 20),
@@ -147,6 +183,7 @@ newer_plot_metric <- function(data, metric, ...) {
       legend.position="bottom",
       legend.box = "horizontal"
     )
+  }
   
   if (metric == "mean_train_mse" || metric == "mean_test_mse") {
     plt <- plt + facet_grid(st_dev ~ type, scales = "free_y", label = "label_parsed")
@@ -308,6 +345,63 @@ plot_metric <- function(data, metric, facet, color, ylabel = "Mean test MSE",
   return(plt)
 }
 
+n <- c(50, 200, 1000)
+p <- c(10, 100, 2000)
+response <- c("linear", "nonlinear")
+metrics <- c("train_mse", "test_mse", "sensitivity", "specificity")
+
+dimensions <- expand.grid(n = n, p = p, response = response)
+
+for (row in 1:nrow(dimensions)) {
+  n <- dimensions$n[row]
+  p <- dimensions$p[row]
+  response <- dimensions$response[row]
+  file <- paste("../../results/monte-carlo-", response, "/aggregate_",
+                response, "_results.rds", sep = "")
+  directory <- paste("../../figures/", response, "-facet/", sep = "")
+
+  results <- readRDS(file)
+
+  for (metric in metrics) {
+    plt <- newer_plot_metric(results, metric, small = FALSE, rescale = FALSE,
+                             n = n, p = p)
+
+    filename <- paste("facet", metric, sep = "_")
+    folder <- sub("_", "-", metric)
+    save_plot(plot = plt,
+              filename = paste(filename, response, n, p, sep = "_"),
+              path = paste(directory, folder, sep = ""))
+  }
+}
+
+
+response <- c("linear", "nonlinear")
+metrics <- c("train_mse", "test_mse", "sensitivity", "specificity")
+
+dimensions <- expand.grid(metric = metrics, response = response)
+publication_directory = "../../figures/publication-facet/"
+
+for (row in 1:nrow(dimensions)) {
+  metric <- dimensions$metric[row]
+  response <- dimensions$response[row]
+  
+  file <- paste("../../results/monte-carlo-", response, "/aggregate_",
+                response, "_results.rds", sep = "")
+  
+  results <- readRDS(file)
+  
+  plt <- newer_plot_metric(results, metric, small = TRUE, rescale = TRUE,
+                     n = 50, p = 2000)
+  
+  filename <- paste("publication_facet", metric, response, "50_2000", sep = "_")
+  save_plot(plot = plt,
+            filename = filename,
+            path = publication_directory,
+            width = 10, height = 10)
+}
+
+stop("STOP")
+
 for (response in 1:2) {
   if (response == 1) {
     aggregate_results <- readRDS("../../results/monte-carlo-linear/aggregate_linear_results.rds")
@@ -367,9 +461,6 @@ for (response in 1:2) {
   
   dimensions <- expand.grid(n = c(50, 200, 1000), p = c(10, 100, 2000))
   
-  plt <- newer_plot_metric(aggregate_results, "mean_test_mse", n = 200, p = 10)
-  
-  stop("T")
   
   # Train MSE
   apply(X = dimensions, MARGIN = 1, FUN = function(row) {
